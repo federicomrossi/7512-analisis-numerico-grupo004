30d4f8a0237530e16ed1d28214cc6d4bf2e5f82e
federicomrossi@gmail.com <federicomrossi@gmail.com@ddab1453-8926-1672-d2c4-b830d0f284ec>
2011-10-01 12:39:15 +0000

    TP1: Importación inicial de archivos

---
+++ TP 1/Archivos/tp1.m
@@ -0,0 +1,23 @@
+% Función que calcula los promedios de las matrices del TP1.
+% POST: se crean dos archivos con formato CSV: uno que contiene los
+% promedios (promediosMatrices.csv) y otro que alberga al error de cada
+% matriz (erroresMatrices.csv). Ambos archivos se crean en el directorio
+% raiz del workspace actual del MATLAB.
+function [] = tp1()
+
+    % Configuración
+    directorio = 'Matrices/';
+    extension = '.txt';
+    matrices = [917 977 1038 1100 1162 1225 1289 1353];
+    dimensionMatrices = size(matrices);
+    cantidadMatrices = dimensionMatrices(1,2);
+    
+    % Calculamos los promedios y los errores
+    [promediosDeMatrices , erroresDeMatrices] = tp1CalculoDePromediosYErrores(matrices, directorio, extension);
+    
+    % Generamos los archivos con formato CSV donde se albergan
+    % los promedios calculados y los errores.
+    csvwrite('promediosMatrices.csv', promediosDeMatrices(1,1:cantidadMatrices));
+    csvwrite('erroresMatrices.csv', erroresDeMatrices(1,1:cantidadMatrices));
+        
+end
