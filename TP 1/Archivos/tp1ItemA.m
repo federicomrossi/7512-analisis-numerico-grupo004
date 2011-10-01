5323e3ec3fc721a09ccc5ea0465bad70138eedbc
federicomrossi@gmail.com <federicomrossi@gmail.com@ddab1453-8926-1672-d2c4-b830d0f284ec>
2011-10-01 12:56:38 +0000

    TP1: Agregados los comentarios en los archivos .m

--- TP 1/Archivos/tp1ItemA.m
+++ TP 1/Archivos/tp1ItemA.m
@@ -1,9 +1,9 @@
-% Función que calcula los promedios de las matrices del TP1.
+% Función que calcula los promedios de las matrices del TP1 (Item A).
 % POST: se crean dos archivos con formato CSV: uno que contiene los
 % promedios (promediosMatrices.csv) y otro que alberga al error de cada
 % matriz (erroresMatrices.csv). Ambos archivos se crean en el directorio
 % raiz del workspace actual del MATLAB.
-function [] = tp1()
+function [] = tp1ItemA()
 
     % Configuración
     directorio = 'Matrices/';
