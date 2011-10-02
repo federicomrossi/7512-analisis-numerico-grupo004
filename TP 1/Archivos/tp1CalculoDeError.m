4afafc2c5ad456179028431b1985e6efbf25cf41
federicomrossi@gmail.com <federicomrossi@gmail.com@ddab1453-8926-1672-d2c4-b830d0f284ec>
2011-10-02 00:22:26 +0000

    TP1: Actualización del informe

--- TP 1/Archivos/tp1CalculoDeError.m
+++ TP 1/Archivos/tp1CalculoDeError.m
@@ -1,14 +1,13 @@
 % Función que calcula el error de una matriz.
-% PRE: se debe pasar por parametro una matriz.
+% PRE: se debe pasar por parametro una matriz y el promedio de sus elementos.
 % POST: se devuelve un escalar representativo del error.
-function [error] = tp1CalculoDeError(matriz,promedio)
+function [error] = tp1CalculoDeError(matriz, promedio)
 
-    %Con los datos de entrada se calcula el error (desviacion
-    % media estandard)
-    
+    % Con los datos de entrada se calcula el error (desviación
+    % media estandar)
     sumatoria = 0;
     
-    %Mediante dos ciclos for anidados, recorro la totalidad de la matriz
+    % Mediante dos ciclos for anidados, recorro la totalidad de la matriz
     % realizando la sumatria de los cuadrados de la diferencia entre el 
     % valor (i,j) de la matriz y el promedio de la misma.
     for i = 1: size(matriz,1)
