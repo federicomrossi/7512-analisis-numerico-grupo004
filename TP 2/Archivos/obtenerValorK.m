62aa3d098ab37edae395b93fb40ab9f37398c672
federicomrossi@gmail.com <federicomrossi@gmail.com@ddab1453-8926-1672-d2c4-b830d0f284ec>
2011-11-27 04:53:31 +0000

    TP2: Actualizacion de metodo de Euler.

--- TP 2/Archivos/obtenerValorK.m
+++ TP 2/Archivos/obtenerValorK.m
@@ -11,8 +11,21 @@ function matrizResultado = obtenerValorK(t)
     datosDeK = load(strcat(directorio, archivo));
     
     % Buscamos el valor de K para el tiempo t dado
-    i = datosDeK == t;
-    matrizResultado = [t datosDeK(i,2)];
+    lenDatosDeK = size(datosDeK);
+    posT = 0;
+    
+    for i = 1:(lenDatosDeK(1,1)+1)
+        if(isequal(datosDeK(i,1),t))
+            posT = i;
+            break;        
+        end
+    end
+
+    if(posT == 0) 
+        MException('Función obtenerValorK: El valor t del intervalo no se encuentra en la tabla');
+    end
+    
+    matrizResultado = [t datosDeK(posT,2)];
    
 end
 
