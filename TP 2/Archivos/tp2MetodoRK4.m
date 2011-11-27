bf8ac48020653e2c75567d957d4d866f16344ab2
federicomrossi@gmail.com <federicomrossi@gmail.com@ddab1453-8926-1672-d2c4-b830d0f284ec>
2011-11-27 15:55:28 +0000

    TP2: Solucionado el problema con RK4. Necesita revisión.

--- TP 2/Archivos/tp2MetodoRK4.m
+++ TP 2/Archivos/tp2MetodoRK4.m
@@ -7,7 +7,7 @@ function tp2MetodoRK4()
     a = 0;
     b = 120;
     U0= 10^10;
-    h = [4,2,1];
+    h = [4, 2, 1, 0.5];
 
     % Procesamos utilizando el método de RK4
     disp(' ');
@@ -18,15 +18,21 @@ function tp2MetodoRK4()
     
     disp(strcat('Procesando para el paso h=', num2str(h(1,2)), '...'));
     rk2 = metodoRK4(funcion, a , b , U0 , h(1,2));
+   
+    disp(strcat('Procesando para el paso h=', num2str(h(1,3)), '...'));
+    rk3 = metodoRK4(funcion, a , b , U0 , h(1,3));
     
-    
+    disp(strcat('Procesando para el paso h=', num2str(h(1,4)), '...'));
+    rk4 = metodoRK4(funcion, a , b , U0 , h(1,4));
     
     % Generamos los archivos con formato CSV donde se albergan los
     % valores obtenidos para cada paso
     disp('Generando archivos CSV de datos...');
-    dlmwrite('tp2MetodoRK4ValoresPaso4.csv', e1,'precision',16);
-    dlmwrite('tp2MetodoRK4ValoresPaso2.csv', e2,'precision',16);
-   
+    dlmwrite('tp2MetodoRK4ValoresPaso4.csv', rk1,'precision',16);
+    dlmwrite('tp2MetodoRK4ValoresPaso2.csv', rk2,'precision',16);
+    dlmwrite('tp2MetodoRK4ValoresPaso1.csv', rk3,'precision',16);
+    dlmwrite('tp2MetodoRK4ValoresPaso05.csv', rk4,'precision',16);
+    
     disp('El proceso ha finalizado exitosamente.');
     disp(' ');
     
