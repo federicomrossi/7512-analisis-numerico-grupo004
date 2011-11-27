52b47b854c4d564650f2a55810640e9e0f194b6a
federicomrossi@gmail.com <federicomrossi@gmail.com@ddab1453-8926-1672-d2c4-b830d0f284ec>
2011-11-27 23:32:56 +0000

    TP2: Actualización de busquedaBinaria.m

--- TP 2/Archivos/busquedaBinaria.m
+++ TP 2/Archivos/busquedaBinaria.m
@@ -1,19 +1,39 @@
+% Función que implementa la busqueda binaria para la busqueda de un valor
+% pasado por parámetro.
+%
+%       busquedaBinaria(vector,valor,max,min)
+%
+% PRE-CONDICIONES
+% vector: es 
+% valor: es el valor que se desea buscar en el vector;
+% max: 
+function indice = busquedaBinaria(vector,valor)
 
-function indice = busquedaBinaria (vector,valor,max,min)
+    max = size(vector);
+    max = max(1,2)
+    min = 1;
+    
+    indice = privateBusquedaBinaria(vector,valor,max,min);
 
-
-if (max < min)
-    indice = -1;
-    return
 end
 
-medio = ceil((max + min)/2);
+% Función de uso privado para usar la recursividad en la busqueda.
+function indice = privateBusquedaBinaria(vector,valor,max,min)
 
-if (vector(medio) < valor)
-    busquedaBinaria (vector,valor,max,medio+1)
-else if (vector(medio) > valor)
-    busquedaBinaria (vector,valor,medio-1,min)
-    else indice = medio;
+    if (max < min)
+        indice = -1;
         return
+    end
+
+    medio = ceil((max + min)/2);
+
+    if (vector(medio) < valor)
+        busquedaBinaria (vector,valor,max,medio+1)
+    else
+        if (vector(medio) > valor)
+            busquedaBinaria (vector,valor,medio-1,min)
+        else indice = medio;
+            return
+        end
     end
 end
