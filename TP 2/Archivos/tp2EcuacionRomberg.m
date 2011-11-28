e75e09ae6d795551b44aa83751c2ffd6320a2a94
federicomrossi@gmail.com <federicomrossi@gmail.com@ddab1453-8926-1672-d2c4-b830d0f284ec>
2011-11-28 01:48:14 +0000

    TP2: Actualización de tp2MetodoDeRomberg.m

--- TP 2/Archivos/tp2EcuacionRomberg.m
+++ TP 2/Archivos/tp2EcuacionRomberg.m
@@ -1,5 +1,13 @@
 % Función que modela la ecuación que rige la cantidad de neutrones 
 % en un reactor nuclear, a través de una tabla de valores en el tiempo
+%
+%		tp2EcuacionRomberg(t)
+%
+% PRE-CONDICIONES
+% t: tiempo en el cual se desea saber la cantidad de neutrones.
+%
+% POST-CONDICIONES
+% Devuelve el numero que representa la cantidad de neutrones en el tiempo t.
 function res = tp2EcuacionRomberg(t)
     
     format longG
@@ -10,7 +18,8 @@ function res = tp2EcuacionRomberg(t)
     % Abrimos el archivo y generamos una matriz con los datos extraidos
     datosDeN = dlmread(strcat(directorio, archivo),',');
     
-    posT = find(datosDeN(:,1)== t);
+    posT = datosDeN(:,1)== t;
     
-    res = datosDeN (posT,2);
+    res = datosDeN(posT,2);
+    
 end
--- TP 2/Archivos/tp2MetodoDeRomberg.m
+++ TP 2/Archivos/tp2MetodoDeRomberg.m
@@ -1,7 +1,8 @@
 % Función que utiliza el Método de Integración de Romberg y lo aplica a 
 %la función propuesta en el TP2.
 % 
-
+% POST-CONDICIONES
+% Se muestra por consola el resultado de la integración.
 function tp2MetodoDeRomberg()
     
     format longG
@@ -12,6 +13,14 @@ function tp2MetodoDeRomberg()
     b = 72;
     K = 5;
     
-    r1 = metodoDeRomberg(funcion,a,b,K)
+    % Procesamos
+    disp(' ');
+    disp('Iniciando aplicación del Método de Romberg.')
+    
+    r = metodoDeRomberg(funcion, a, b, K);
+    
+    disp('El proceso ha finalizado exitosamente.');
+    disp(' ');
+    disp(['El resultado de la integración es ' num2str(r)]);
     
 end
